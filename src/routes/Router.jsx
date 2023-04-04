f073673915f39a86a07585a27250f716d24aaa20
Võ Văn Kiệt <vovankiet01042001@gmail.com>
2023-04-04 09:29:45 +0700

    update

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
 import AdminGuard from "guards/AdminGuard";
 import { AdminLayout } from "layouts/admin/Admin";
 import ElearningManagement from "pages/admin/ElearningManagement";
@@ -17,23 +16,11 @@ import Register from "pages/register/Register";
 import UserForm from "pages/use-form/UserForm";
 import UserInfo from "pages/user-info/UserInfo";
 import UserFormAmin from "pages/userFormAdmin/UserFormAdmin";
-=======
-import { useRoutes } from 'react-router-dom';
-import NoAuthGuard from 'guards/NoAuthGuard';
-import HomeLayout from 'layouts/home/HomeLayout';
-import CourseDetailPage from 'pages/course-detail/CourseDetailPage';
-import HomePage from 'pages/home/HomePage';
-import Login from 'pages/login/Login';
-import Register from 'pages/register/Register';
-import UserForm from 'pages/use-form/UserForm';
-import UserInfo from 'pages/user-info/UserInfo';
-import CourseCatalory from 'pages/course-catalory/CourseCatalory';
->>>>>>> bf0169b5542e2a7753398b18a0dabdf69117bba8
+import CourseCatalory from "pages/course-catalory/CourseCatalory";
 
 export default function Router() {
   const routing = useRoutes([
     {
-<<<<<<< HEAD
       path: "/",
       element: <HomeLayout />,
       children: [
@@ -62,47 +49,11 @@ export default function Router() {
           path: "/user-form",
           element: <UserForm />,
         },
+        {
+          path: "/course-catalory",
+          element: <CourseCatalory />,
+        },
       ],
-=======
-        path: "/",
-        element: <HomeLayout/>,
-        children: [
-            {
-                path: "/",
-                element: <HomePage/>,
-            },
-            {
-                path: "/course-detail/:id",
-                element: <CourseDetailPage/>,
-            },
-            {
-                path: "/",
-                element: <NoAuthGuard/>,
-                children: [
-                    {path: "/login",
-                    element: <Login/>,
-                    },
-                ],
-            },
-            {
-                path: "/register",
-                element: <Register/>,
-            },
-            {
-                path: "/user-info",
-                element: <UserInfo/>,
-            },
-            {
-                path: "/user-form",
-                element: <UserForm/>,
-            },
-            {
-                path: "/course-catalory",
-                element: <CourseCatalory/>,
-                
-            }
-        ],
->>>>>>> bf0169b5542e2a7753398b18a0dabdf69117bba8
     },
     {
       path: "/admin",
